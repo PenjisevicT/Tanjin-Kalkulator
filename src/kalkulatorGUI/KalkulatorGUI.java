5dbb9619579a77032f66f9e0cfa1f3a7f20cc3a2
Dragan <neki@tamo.mejl>
2019-01-04 19:26:56 +0100

    Max limit na broj cifri

--- src/kalkulatorGUI/KalkulatorGUI.java
+++ src/kalkulatorGUI/KalkulatorGUI.java
@@ -76,8 +76,7 @@ public class KalkulatorGUI extends JFrame {
 		btn1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "1");
+				unosBroja("1", textArea);
 			}
 		});
 		buttonGroup.add(btn1);
@@ -90,8 +89,7 @@ public class KalkulatorGUI extends JFrame {
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "2");
+				unosBroja("2", textArea);
 			}
 		});
 		btn2.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -102,8 +100,7 @@ public class KalkulatorGUI extends JFrame {
 		btn3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "3");
+				unosBroja("3", textArea);
 			}
 		});
 		btn3.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -114,8 +111,7 @@ public class KalkulatorGUI extends JFrame {
 		btn4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e)
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "4");
+				unosBroja("4", textArea);
 			}
 		});
 		btn4.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -126,8 +122,7 @@ public class KalkulatorGUI extends JFrame {
 		btn5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "5");
+				unosBroja("5", textArea);
 			}
 		});
 		btn5.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -138,8 +133,7 @@ public class KalkulatorGUI extends JFrame {
 		btn6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "6");
+				unosBroja("6", textArea);
 			}
 		});
 		btn6.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -150,8 +144,7 @@ public class KalkulatorGUI extends JFrame {
 		btn7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "7");
+				unosBroja("7", textArea);
 			}
 		});
 		btn7.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -162,8 +155,7 @@ public class KalkulatorGUI extends JFrame {
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e)
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "8");
+				unosBroja("8", textArea);
 			}
 		});
 		btn8.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -174,8 +166,7 @@ public class KalkulatorGUI extends JFrame {
 		btn9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "9");
+				unosBroja("9", textArea);
 			}
 		});
 		btn9.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -186,8 +177,7 @@ public class KalkulatorGUI extends JFrame {
 		btn0.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
-				String s = textArea.getText();
-				textArea.setText(s + "0");
+				unosBroja("0", textArea);
 			}
 		});
 		btn0.setFont(new Font("Tahoma", Font.BOLD, 18));
@@ -325,5 +315,71 @@ public class KalkulatorGUI extends JFrame {
 		btnC.setFont(new Font("Tahoma", Font.BOLD, 18));
 		btnC.setBounds(296, 63, 50, 50);
 		contentPane.add(btnC);
+	}
+	
+	public void unosBroja(String broj, JTextArea txtPolje)
+	{
+		String s = txtPolje.getText();
+		
+		//Ispod je ovo detaljno objasnjenjo :D
+		if (s.matches(".+[+*/-]\\n")) //Specijalan slucaj, kada treba prva cifra drugog broja da se unese, onda je 
+		{                             //odmah unosimo jer nema potrebe da proveravamo koliko je dugacak, kada je vec prva :) 
+			txtPolje.setText(s + broj);
+			return;     //Odmah se vracamo, nema sta dalje da radimo
+		}
+		
+		/*
+		 * Treba da proverimo koliko je broj dugacak da bi videli moze li korisnik da unese jos cifara. 
+		 * Ideja je sledeca. Znamo da ce eventualno nas izraz da izgleda ovako "34+5". Znaci mi treba da podelimo to na
+		 * "34" i "5" da bi mogli da proverimo duzinu svako broja posebno. Delicemo broj string metodom split(). Ona od jednog
+		 * stringa napravi niz stringova kada joj kazemo sta joj je granicnik. Na primer "34+5".split("+") nam da niz {"34", "5"}
+		 * Posto znamo da nas niz moze da ima jedan (dok korisnik kuca prvi broj) ili dva clana (kada korisnik kuca drugi) moramo
+		 * da znamo koji od ta dva broja treutno gledamo.
+		 */ 
+		
+		int clan = 0; //Indeks u nizu. Stavljamo ga odmah na nulu jer ce, u pocetku, dok korisnik kuca prvi broj bude prvi clan 
+		             //u nizu. Na primer, na samom startu nemamo nista, "".split("+") ce da kaze a u ja nemam "+" nigde tu, evo tebi
+		             //nazad sve, pa nam da { "" }. Onda korisnik kuca 3, dobijemo "3", opet, "3".split("+") bude { "3" }... Sve dokle
+		             //korisnik ne otkuca neku operaciju mi imamo samo 1 clan na indeksu 0 i njega gledamo.
+		
+		int imaDec = 0; //Dodatna stvar. Imamo brojeve sa ostatkom. Ako korisnik moze da ukuca pet cifri znaci "12345" ali i
+		                //"123.45". Mora da obratimo paznju na to postoji li . u broju, i ako postoji a hocemo da moze samo da se unese
+		                //5 cifri onda moramo da kazemo ok imas tacku, onda 6 karaktera sveukupno. int je, a ne boolean, zato sto
+		                //ovako olaksa posao. Posle je samo postavimo na 1 ako ima ., pa onda kazem kucaj dok je manje od 5 + imaDec.
+		               //Ako je nema, bude 0, pa idemo do 5, a ako je ima onda smo na 6 :)
+		
+		//Sada moramo da proverimo da li gledamo prvi ili drugi broj koji korisnik unosi. matches() ce da mi kaze da li se string 
+		//koji gledamo poklapa sa onim u zagradama. Taj uvrnuti izraz u zagradama je regex, regular expression. Prica sama za sebe :)
+		//Jos o tome na https://en.wikipedia.org/wiki/Regular_expression
+		//U kratkim crtama, mozemo da mu objasnimo sta da trazi vrlo vrlo detaljno.
+		//Prevod:
+		//       s.matches             ("         .+                                   [+*/-]                          \\n               .+")
+		//Ako je string s jednak sa:          Pocinje sa bilo sta         pa onda imamo + ili - ili / ili -           pa novi red      pa bilo sta
+		
+		//Zasto novi red? Pa, nas digitron, kada upisuje operaciju, kada korisnik otkuca "34+5"
+		//On to hoce da prikaze kao
+		//34+
+		//5
+		//Pa stavi "34+\n5" i mi to moramo da navedemo ovde 
+		
+		
+		if (s.matches(".+[+*/-]\\n.+"))  //Ako se ovo poklapa znaci da mi gledamo u nesto kao "34+5", samim tim nas intresuje drugi clan
+		{                                //tj. 5, pa stavljamo indeks na 1
+			clan = 1;
+		}
+		
+		//Jos nam je ostalo da proverimo da li imamo tacku negde u tom broju
+		//Prevod:
+		//      .+                            \\.                .+
+		//  Pocinje sa bilo sta         pa ima tacku        i zavrsava se sa bilo sta
+		if (s.split("[+*/-]\\n")[clan].matches(".+\\..+"))
+		{
+			imaDec = 1;
+		}
+		
+		if (s.split("[+*/-]\\n")[clan].length() < 5 + imaDec)  //Na kraju samo proveravamo jesmo li probili limit od 5 (plus tacka ako je ima)
+		{
+			txtPolje.setText(s + broj);       //Ako nismo, upisujemo novu cifru, ako jesmo ne desava se ama nista
+		}
 	}
 }
